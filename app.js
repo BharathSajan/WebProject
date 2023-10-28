6405c3a11fb116992aef5e638b2a5b93fa1658aa
BharathSajan <bharath.sajan@gmail.com>
2023-10-28 20:12:14 +0530

    authentication set

--- app.js
+++ app.js
@@ -5,15 +5,26 @@ const app = express();
 const port = 4000;
 const passport = require('./auth');
  require('./auth');
- app.use(express.static('views'));
+
+ app.use(session({
+   secret: "cats",
+  resave: false,
+saveUninitialized:false,
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(express.static('views'));
+
+//database utilities
+const {isUserInDatabase, addUserToDatabase} = require('./dbfunctions');
 
 function isLoggedIn(req,res, next){
+  // console.log(req.user);
   req.user ? next(): res.sendStatus(401);
 }
 
-app.use(session({ secret: "cats"}));
-app.use(passport.initialize());
-app.use(passport.session());
+
 
 app.get('/', (req, res) => {
     // Send the 'signinPage.html' as the response
@@ -23,9 +34,11 @@ app.get('/', (req, res) => {
 app.get('/auth/google',//this page is the portal for google
   passport.authenticate('google', { scope: ['email','profile']})//scope refers to the application permissions we are requesting for from the user's google account.Here we ask for user's email and basic profile information.
 ) 
+
 app.get('/google/callback',//this page handles the result of the authentication at the google portal
   passport.authenticate('google',{
-    successRedirect: '/landing',
+    
+    successRedirect: '/studentspage',
     failureRedirect: '/auth/failure',
 }));
 
@@ -33,9 +46,33 @@ app.get('/auth/failure',(req,res)=>{
 res.send('something went wrong. .');
 });
 
-app.get('/studentspage',isLoggedIn,(req, res)=>{
-  res.sendFile(__dirname + '/studentsPage.html')
+app.get('/studentspage', isLoggedIn, (req, res) => {
+  // Gather details of the user
+  const userEmail = req.user.email;
+  const userName = req.user.name['givenName'] + ' ' + req.user.name['familyName'];
+  const user = { name: userName, email: userEmail };
+
+  isUserInDatabase(userEmail, function (err, userExists) {
+    if (err) {
+      res.sendStatus(500); // Handle database error
+    } else if (userExists) {
+      // User is in the database, redirect to '/landing'
+      res.redirect('/landing');
+    } else {
+      // User is not in the database, add them to the database
+      addUserToDatabase(user, function (err) {
+        if (err) {
+          console.log(err);
+          res.sendStatus(500); // Handle database insertion error
+        } else {
+          // User added to the database, send the studentsPage.html
+          res.sendFile(__dirname + '/studentsPage.html');
+        }
+      });
+    }
+  });
 });
+
 app.get('/landing',isLoggedIn,(req, res)=>{
   res.sendFile(__dirname + '/studentChannelPage.html')
 });
