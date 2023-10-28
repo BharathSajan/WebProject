5ee6a19fbb336c146db884444356f2d311274f74
ClintJoseph <28clintjoseph@gmail.com>
2023-10-28 12:06:29 +0530

    dbfunctions.js modify

--- dbfunctions.js
+++ dbfunctions.js
@@ -12,7 +12,7 @@ function getAllUsers(callback) {
 }
 
 function getintrestedCommunities(userid,callback){
-    db.all('SELECT community.id as Id,community.title as Title from community,user_tags,community_tags where(user_tags.uid = ? and user_tags.tid = community_tags.tid and community_tags.cid = community.id)',userid,(err, rows) => {
+    db.all('SELECT community.id as Id,community.title as Title,Date as community.create_date from community,user_tags,community_tags where(user_tags.uid = ? and user_tags.tid = community_tags.tid and community_tags.cid = community.id)',userid,(err, rows) => {
         if (err) {
             callback(err, null);
         } else {
@@ -22,3 +22,36 @@ function getintrestedCommunities(userid,callback){
 
 }
 
+function addUserToDatabase(user, callback) {
+    // user parameter should be an object with properties like name, email, etc.
+    const { name, email } = user;
+
+    // Execute a query to insert a new user into the "users" table
+    db.run('INSERT INTO users (name, email) VALUES (?, ?)', [name, email], function (err) {
+        if (err) {
+            callback(err, null); // An error occurred
+        } else {
+            // User was successfully inserted; this.lastID contains the new user's ID
+            callback(null, this.lastID);
+        }
+    });
+}
+
+function isUserInDatabase(username) {
+    // Execute a query to check if the user exists in the database
+    db.get('SELECT * FROM users WHERE name = ?', username, (err, row) => {
+        if (err) {
+            console.log(err)// An error occurred
+        } else {
+            if (row) {
+                // User exists in the database
+                return true;
+            } else {
+                // User does not exist in the database
+                return false;
+            }
+        }
+    });
+}
+
+module.exports = { isUserInDatabase, addUserToDatabase };
